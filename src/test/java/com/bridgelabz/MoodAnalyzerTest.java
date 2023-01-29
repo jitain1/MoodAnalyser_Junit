4ba21ec62e82897c9598bb17cc847c48f26f1880
jitain1 <singhjitainj@gmail.com>
2023-01-30 00:09:04 +0530

    added: test case for return sad

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
 
-public class MoodAnalyzerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyzerTest {
+	@Test
+	public void givenMessageWhenSadShouldReturnSad() {
+		MoodAnalyzer moodanalyzer = new MoodAnalyzer();
+		String actualOutput = moodanalyzer.analyzeMood("I am in sad mood");
+		Assert.assertEquals("SAD", actualOutput);
+	}
 }
