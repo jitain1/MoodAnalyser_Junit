187cdd7b01db5e7c0aacf55637261ee3a7746787
jitain1 <singhjitainj@gmail.com>
2023-01-30 00:33:07 +0530

    added: parameterized constructor

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,19 +1,33 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
+	String message;
 
+/* Parameterized constructor */
+	public MoodAnalyzer(String message) {
+		this.message = analyzeMood(message);
+	}
+	
+/* Default constructor*/	
+	public MoodAnalyzer() {
+	}
+
+/* this method will return Mood(HAPPY/SAD)*/
 	public String analyzeMood(String message) {
-        if (message.toLowerCase().contains("sad")) {
-            return "SAD";
-        } else
-            return "HAPPY";
-    }
+		if (message.toLowerCase().contains("sad"))
+			message = "SAD";
+		else
+			message = "HAPPY";
+		return message;
+	}
 
+	
 	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyzer");
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		
-		System.out.println(moodAnalyzer.analyzeMood("I'm in Sad mood!!"));
-		System.out.println(moodAnalyzer.analyzeMood("I'm happy!!"));
+
+		System.out.println("Mood is " + moodAnalyzer.analyzeMood("I'm in Sad mood!!!"));
+		System.out.println("Mood is " + moodAnalyzer.analyzeMood("I'm happy!!!"));
 	}
 
 }
